b2712fe1884d84c70ecf2a5afebe689595283462
Eduardo Pooter Reis <edupooterreis@gmail.com>
2022-05-03 12:19:05 -0300

    (feat) Added newmath lib; Show the square root.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/edupooter/ola
 
 go 1.18
+
+require github.com/edupooter/newmath v0.0.0-20220503150523-400947c708c4
--- ola.go
+++ ola.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/edupooter/newmath"
+)
 
 func main() {
-	fmt.Printf("Olá, mundo.\n")
+	fmt.Printf("Olá, mundo.\n")                   // default
+	fmt.Printf("Sqrt(2) = %v\n", newmath.Sqrt(2)) // another
 }
